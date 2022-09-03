20847e2812e6f7db7b58384aaf1ae688626337be
Damián Silvani <munshkr@gmail.com>
2022-09-03 10:58:41 -0300

    feat: Allow configuring ports and verbose mode on bridge script

--- bin/hydra-sc-bridge.js
+++ bin/hydra-sc-bridge.js
@@ -1,25 +1,39 @@
 #!/usr/bin/env node
 
 const OSC = require('osc-js')
-const packageInfo = require('../package.json');
+const program = require('commander');
+const packageInfo = require('../package.json')
 
-const config = {
-  wsServer: { port: 8080 },
-  udpClient: { port: 9129 },
-  udpServer: { port: 9130 },
+program
+  .version(packageInfo.version)
+  .option('-p, --port <number>', 'WebSocket port', 8080)
+  .option('--osc-port <number>', 'OSC port', 9130)
+  .option('-v, --verbose', 'Be verbose (print OSC messages)', false)
+  .parse(process.argv)
+
+const options = program.opts();
+
+console.log(`hydra-superdirt ${packageInfo.version} - OSC-WebSocket bridge`)
+
+const osc = new OSC({
+  plugin: new OSC.BridgePlugin({
+    wsServer: { port: options.port },
+    udpServer: { port: options.oscPort },
+  })
+})
+
+if (options.verbose) {
+  console.log("Verbose is ON. It will print *all* OSC messages, so performance will be degraded.")
+  osc.on('/rms', message => {
+    console.log("RMS", message.args)
+  })
 }
-const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
-
-// osc.on('/rms', message => {
-//     console.log("RMS", message.args)
-// })
 
 osc.on('open', () => {
   console.log(
-    `Listening on port ${config.wsServer.port}. ` +
-    `Will redirect RMS messages to UDP port ${config.udpServer.port}.`
+    `Listening on port ${options.port}. ` +
+    `Will redirect RMS messages to UDP port ${options.oscPort}.`
   )
 })
 
-console.log(`hydra-superdirt ${packageInfo.version} - OSC-WebSocket bridge`)
 osc.open()
